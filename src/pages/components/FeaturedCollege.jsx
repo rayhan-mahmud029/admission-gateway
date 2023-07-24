040780108db08cc187bd8e1e37f73d358df14015
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-07-24 23:58:27 +0600

    isssue fixed

--- src/pages/components/FeaturedCollege.jsx
+++ src/pages/components/FeaturedCollege.jsx
@@ -18,7 +18,7 @@ const FeaturedCollege = () => {
             <h2 className="text-center text-2xl text-orbitron my-4 font-bold">Featured Colleges</h2>
 
             <div className="grid grid-cols-3 gap-3">
-                {
+                {/* {
                     colleges.map(college => {
                         <div className="card w-96 bg-base-100 shadow-xl" key={college._id}>
                             <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
@@ -35,7 +35,7 @@ const FeaturedCollege = () => {
                             </div>
                         </div>
                     })
-                }
+                } */}
             </div>
 
         </div>
