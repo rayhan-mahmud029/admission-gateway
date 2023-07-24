44ce3f88ec8047247c724dc898722858f6e61309
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-07-24 23:56:34 +0600

    deploy test

--- src/pages/components/FeaturedCollege.jsx
+++ src/pages/components/FeaturedCollege.jsx
@@ -9,6 +9,7 @@ const FeaturedCollege = () => {
             .then(data => {
                 console.log(data);
                 setColleges(data)
+                console.log(colleges);
             })
     }, [])
 
@@ -18,7 +19,22 @@ const FeaturedCollege = () => {
 
             <div className="grid grid-cols-3 gap-3">
                 {
-                    
+                    colleges.map(college => {
+                        <div className="card w-96 bg-base-100 shadow-xl" key={college._id}>
+                            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                    })
                 }
             </div>
 
